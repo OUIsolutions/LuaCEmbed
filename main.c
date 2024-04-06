c0617069246c3d811433250216f43342065dcc64
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-06 16:25:21 -0300

    at

--- main.c
+++ main.c
@@ -4,13 +4,34 @@
 
 
 
-int nada(lua_State *l){
-    
+int nada(lua_State *L){
+
+
+    char *valor = (char*)lua_touserdata(L,lua_upvalueindex(1));
+    printf("chamou %s\n",valor);
 
     
 
-    printf("chamou\n");
+    // Itera sobre os elementos da tabela
+    lua_pushnil(L); // Coloca a chave nula na pilha
+    while (lua_next(L, 1) != 0) { // Enquanto houver elementos na tabela
+        // Obtém a chave e o valor atual da tabela
+        if (lua_isstring(L, -2)) {
+            const char *chave = lua_tostring(L, -2);
+            if (lua_isstring(L, -1)) {
+                const char *valor = lua_tostring(L, -1);
+                printf("%s: %s\n", chave, valor);
+            } else {
+                printf("%s: valor não é uma string\n", chave);
+            }
+        } else {
+            printf("chave não é uma string\n");
+        }
+        lua_pop(L, 1); // Remove o valor, mantendo a chave na pilha para a próxima iteração
+    }
+
     return 0;
+
     
 }
 
@@ -18,8 +39,12 @@ int nada(lua_State *l){
 int main(){
     lua_State *L = luaL_newstate(); // Inicializa o estado Lua
 
-    lua_pushcfunction(L, nada);
+    const char *teste = "aaaaaaaa";
+    lua_pushlightuserdata(L,(void*)teste);
+    lua_pushcclosure(L,nada,1);
     lua_setglobal(L, "nada");
+
+   
 
     luaL_dofile(L,"teste.lua");
 
--- main2.c
+++ main2.c
@@ -1,11 +1,10 @@
+
 #include <stdio.h>
+#include <pthread.h>
 
-#include <math.h>
 
-int main(){
-
-    printf("flor: %lf\n",floor(30));
-    printf("pow: %lf\n",pow(30,30));
-    printf("fmod: %lf\n",fmod(20,20));
-
+int main() {
+    
+    LuaCEmbeed *created = newLa
+    return 0;
 }
