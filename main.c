4254d8b249cd0e6d1325495bef9f7214d633b146
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-06 16:45:39 -0300

    att

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 
 #include "src/one.c"
 
+void iterate(lua_State *L,int index){
+    // Itera sobre os elementos da tabela
+    lua_pushnil(L); // Coloca a chave nula na pilha
+    while (lua_next(L, index) != 0) { // Enquanto houver elementos na tabela
+        // Obtém a chave e o valor atual da tabela
+        if (lua_isstring(L, -2)) {
+            const char *chave = lua_tostring(L, -2);
+            printf("%s:\n{",chave);
 
+            if (lua_isstring(L, -1)) {
+                const char *valor = lua_tostring(L, -1);
+                printf("%s\n", valor);
+            } 
+
+            else if (lua_istable(L,-1)) {
+                int index =lua_gettop(L);
+                
+                printf("index %d\n",index); 
+                iterate(L, index);
+            }
+            printf("\n}");
+
+        } else {
+            printf("chave não é uma string\n");
+        }
+        lua_pop(L, 1); // Remove o valor, mantendo a chave na pilha para a próxima iteração
+    }
+
+}
 
 
 int nada(lua_State *L){
@@ -10,25 +38,8 @@ int nada(lua_State *L){
     char *valor = (char*)lua_touserdata(L,lua_upvalueindex(1));
     printf("chamou %s\n",valor);
 
+    iterate(L,2);
     
-
-    // Itera sobre os elementos da tabela
-    lua_pushnil(L); // Coloca a chave nula na pilha
-    while (lua_next(L, 1) != 0) { // Enquanto houver elementos na tabela
-        // Obtém a chave e o valor atual da tabela
-        if (lua_isstring(L, -2)) {
-            const char *chave = lua_tostring(L, -2);
-            if (lua_isstring(L, -1)) {
-                const char *valor = lua_tostring(L, -1);
-                printf("%s: %s\n", chave, valor);
-            } else {
-                printf("%s: valor não é uma string\n", chave);
-            }
-        } else {
-            printf("chave não é uma string\n");
-        }
-        lua_pop(L, 1); // Remove o valor, mantendo a chave na pilha para a próxima iteração
-    }
 
     return 0;
 
