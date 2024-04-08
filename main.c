71c0f0fc2e4e524b5c23c823b6f1c086f879276a
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-08 07:32:01 -0300

    at

--- main.c
+++ main.c
@@ -4,15 +4,8 @@
 LuaCEmbedReturn  * nada(LuaCEmbed *self){
 
 
-    const char * teste =  "function extrai(f)\n return f['a'] \nend";
-    LuaCEmbed_evaluate_string(self,teste);
-
-    lua_getglobal(self->state, "extrai");
-    lua_pushvalue(self->state,1);
-
-    lua_pcall(self->state,1,1,0);
-
-    printf("valor: %s\n", lua_tostring(self->state,-1));
+    printf("%d\n", LuaCEmbed_get_table_arg_type(self,0,"['a']"));
+    printf("%s\n", lua_tostring(self->state,-1));
 
     return NULL;
 
--- src/constants/args.h
+++ src/constants/args.h
@@ -1,6 +1,6 @@
 #define LUA_CEMBED_INVALID_ARGS -1
 
 #define PRIVATE_LUA_CEMBED_FUNCTION_EVALUATION_NAME "private_lua_c_embed_evaluation"
-#define PRIVATE_LUA_CEMBED_FUNCTION_EVALUATION_CODE "function %s(f)\n %s \n end\n"
+#define PRIVATE_LUA_CEMBED_FUNCTION_EVALUATION_CODE "function %s(f)\n return f%s \n end\n"
 
 #define LUA_CEMBED_ARGS_BUFFER_SIZE 2000
