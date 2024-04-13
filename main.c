fc76556d8fe9f7abe4dcaf0960542d63522f2d42
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-13 01:10:06 -0300

    at

--- main.c
+++ main.c
@@ -1,23 +1,10 @@
 
 #include "src/one.c"
 
-<<<<<<< HEAD
-LuaCEmbedReturn  * nada(LuaCEmbed *self){
+LuaCEmbedResponse * print_lua_value(LuaCEmbed *self){
 
 
-    printf("int arg %d\n", LuaCEmbed_get_arg_type(self,0));
-
-=======
-LuaCEmbedResponse * print_lua_value(LuaCEmbed *values){
-    if(LuaCEmbed_get_arg_type(values,0) == LUA_CEMBED_NUMBER){
-        printf("%lf\n", LuaCEmbed_get_double_arg(values,0));
-    }
->>>>>>> origin/table_getting
-
-    if(LuaCEmbed_get_arg_type(values,0) == LUA_CEMBED_STRING){
-        printf("%s\n", LuaCEmbed_get_str_arg(values,0));
-    }
-    return NULL;
+    
 
 }
 
