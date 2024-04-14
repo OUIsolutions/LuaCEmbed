ede5466c58131776cf2890a2bbe690167e448d2c
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-14 05:21:12 -0300

    at

--- src/LuaCEmbed/args/table/table.h
+++ src/LuaCEmbed/args/table/table.h
@@ -1,6 +1,18 @@
 
 
+int private_LuaCEmbed_ensure_table_arg_type(LuaCEmbed *self,int index,int expected_type);
+
 int  LuaCEmbed_get_table_arg_type(LuaCEmbed *self, int index,const char *expresion,...);
 
 
 char *  LuaCEmbed_get_table_arg_string(LuaCEmbed *self, int index,const char *expresion,...);
+
+long LuaCEmbed_get_table_arg_long(LuaCEmbed *self, int index,const char *expresion,...);
+
+long LuaCEmbed_get_table_arg_size(LuaCEmbed *self, int index,const char *expresion,...);
+
+
+double LuaCEmbed_get_table_arg_double(LuaCEmbed *self, int index,const char *expresion,...);
+
+bool LuaCEmbed_get_table_arg_bool(LuaCEmbed *self, int index,const char *expresion,...);
+
--- src/constants/errors.h
+++ src/constants/errors.h
@@ -5,3 +5,4 @@
 #define PRIVATE_LUA_CEMBED_ARG_WRONG_TYPE "arg %d at function:%s its %s instead of  %s"
 #define PRIVATE_LUA_CEMBED_GLOBAL_NOT_EXIST "global variable:%s not exist"
 #define PRIVATE_LUA_CEMBED_GLOBAL_WRONG_TYPE "global variable:%s its %s instead of %s"
+#define PRIVATE_LUA_CEMBED_TABLE_ARG_WRONG_TYPE "table-value at index %d of function %s its %s instead of %s"
