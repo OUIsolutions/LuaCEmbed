478838dd1ad0e52d51e213a037f8e336281bb3c0
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-15 21:56:31 -0300

    at

--- src/LuaCEmbed/evaluation/evaluation.c
+++ src/LuaCEmbed/evaluation/evaluation.c
@@ -26,14 +26,10 @@ int LuaCEmbed_evaluete_file(LuaCEmbed *self, const char *file){
 
 }
 
+int private_LuaCEmbed_evaluate_puting_on_top_of_stack(LuaCEmbed *self,char *code, va_list args){
 
-char * LuaCEmbed_evaluate_string_returning_string(LuaCEmbed *self, char *code, ...){
-
-    va_list args;
-    va_start(args,code);
     char formated_expresion[LUA_CEMBED_ARGS_BUFFER_SIZE] = {0};
     vsnprintf(formated_expresion, sizeof(formated_expresion),code,args);
-    va_end(args);
 
     char buffer[LUA_CEMBED_ARGS_BUFFER_SIZE] = {0};
     sprintf(buffer,
@@ -43,24 +39,87 @@ char * LuaCEmbed_evaluate_string_returning_string(LuaCEmbed *self, char *code, .
     );
 
     if(LuaCEmbed_evaluate_string_no_return(self, buffer)){
-        return  NULL;
+        return  LUA_CEMBED_GENERIC_ERROR;
     }
 
     lua_getglobal(self->state,PRIVATE_LUA_CEMBED_EVALUATION_NAME);
 
     int type = lua_type(self->state,-1);
 
-    if(type ==LUA_CEMBED_STRING ){
-        return (char*)lua_tostring(self->state,-1);
-    }
-
     if(type == LUA_CEMBED_FUNCTION){
-        if(lua_pcall(self->state,1,0,0)){
+        if(lua_pcall(self->state,0,1,0)){
             const char *generated_error = lua_tostring(self->state,-1);
             LuaCEmbed_raise_error(self,generated_error );
         }
     }
+    return LUA_CEMBED_OK;
+
+}
+char * LuaCEmbed_evaluate_string_returning_string(LuaCEmbed *self, char *code, ...){
+
+    va_list args;
+    va_start(args,code);
+    int possible_error = private_LuaCEmbed_evaluate_puting_on_top_of_stack(self,code,args);
+    va_end(args);
+    if(possible_error){
+        return  NULL;
+    }
+
     return (char*)lua_tostring(self->state,-1);
 
+}
+
+
+int  LuaCEmbed_get_global_evaluation_type(LuaCEmbed *self, char *code,...){
+    va_list args;
+    va_start(args,code);
+    int possible_error = private_LuaCEmbed_evaluate_puting_on_top_of_stack(self,code,args);
+    va_end(args);
+    if(possible_error){
+        return  LUA_CEMBED_GENERIC_ERROR;
+    }
+
+    return lua_type(self->state,-1);
+}
+
+
+long LuaCEmbed_get_global_evaluation_size(LuaCEmbed *self, char *code,...){
+
+}
+
+long LuaCEmbed_get_global_evaluation_long(LuaCEmbed *self, char *code,...){
+
+    va_list args;
+    va_start(args,code);
+    int possible_error = private_LuaCEmbed_evaluate_puting_on_top_of_stack(self,code,args);
+    va_end(args);
+    if(possible_error){
+        return  LUA_CEMBED_GENERIC_ERROR;
+    }
+    return (long)lua_tonumber(self->state,-1);
+
+}
+
+double LuaCEmbed_get_global_evaluation_double(LuaCEmbed *self, char *code,...){
+
+    va_list args;
+    va_start(args,code);
+    int possible_error = private_LuaCEmbed_evaluate_puting_on_top_of_stack(self,code,args);
+    va_end(args);
+    if(possible_error){
+        return  LUA_CEMBED_GENERIC_ERROR;
+    }
+    return (double)lua_tonumber(self->state,-1);
+}
+
+bool LuaCEmbed_get_global_evaluation_bool(LuaCEmbed *self, char *code,...){
+    va_list args;
+    va_start(args,code);
+    int possible_error = private_LuaCEmbed_evaluate_puting_on_top_of_stack(self,code,args);
+    va_end(args);
+    if(possible_error){
+        return  LUA_CEMBED_GENERIC_ERROR;
+    }
+    return (bool)lua_toboolean(self->state,-1);
 }
 
--- src/LuaCEmbed/evaluation/evaluation.h
+++ src/LuaCEmbed/evaluation/evaluation.h
@@ -5,10 +5,13 @@ int LuaCEmbed_evaluate_string_no_return(LuaCEmbed *self, const char *str);
 
 int LuaCEmbed_evaluete_file(LuaCEmbed *self, const char *file);
 
+int private_LuaCEmbed_evaluate_puting_on_top_of_stack(LuaCEmbed *self,char *code, va_list args);
+
+char * LuaCEmbed_evaluate_string_returning_string(LuaCEmbed *self, char *code, ...);
+
 
 int  LuaCEmbed_get_global_evaluation_type(LuaCEmbed *self, char *code,...);
 
-char * LuaCEmbed_evaluate_string_returning_string(LuaCEmbed *self, char *code, ...);
 
 long LuaCEmbed_get_global_evaluation_size(LuaCEmbed *self, char *code,...);
 
