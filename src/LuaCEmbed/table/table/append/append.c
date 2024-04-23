d730c433bca695837346ae8fad607c5c30013e0e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-22 23:54:08 -0300

    at

--- src/LuaCEmbed/table/table/append/append.c
+++ src/LuaCEmbed/table/table/append/append.c
@@ -8,3 +8,27 @@ void  LuaCEmbedTable_append_string(LuaCEmbedTable *self,  const char *value){
 
     lua_rawset(self->main_object->state,-3);
 }
+
+void  LuaCEmbedTable_append_long(LuaCEmbedTable *self,  long  value){
+    long size = LuaCEmbedTable_get_size(self);
+    lua_getglobal(self->main_object->state,self->global_name);
+    lua_pushinteger(self->main_object->state,size+1);
+    lua_pushnumber(self->main_object->state,(double)value);
+    lua_rawset(self->main_object->state,-3);
+}
+
+void  LuaCEmbedTable_append_double(LuaCEmbedTable *self, double  value){
+    long size = LuaCEmbedTable_get_size(self);
+    lua_getglobal(self->main_object->state,self->global_name);
+    lua_pushinteger(self->main_object->state,size+1);
+    lua_pushnumber(self->main_object->state,value);
+    lua_rawset(self->main_object->state,-3);
+}
+
+void  LuaCEmbedTable_append_bool(LuaCEmbedTable *self,  bool value){
+    long size = LuaCEmbedTable_get_size(self);
+    lua_getglobal(self->main_object->state,self->global_name);
+    lua_pushinteger(self->main_object->state,size+1);
+    lua_pushboolean(self->main_object->state,value);
+    lua_rawset(self->main_object->state,-3);
+}
