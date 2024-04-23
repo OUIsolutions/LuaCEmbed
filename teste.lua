32efd0a137f79e10131a75cb1aab52b7bfd8d9f9
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-23 08:11:44 -0300

    att

--- teste.lua
+++ teste.lua
@@ -1,12 +1,11 @@
-require("minha_biblioteca")
 v1 = create_obj(200)
 v1.increment(50)
 
-print("num",v1.num)
-print("a",v1.a)
+print("num ",v1.num)
+print("a ",v1.a)
 
-print("valores 1",v1.valores[1])
-print("valores 3",v1.valores[3])
+print("valores 1: ",v1.valores[1])
+print("valores 3: ",v1.valores[3])
 
 
 
