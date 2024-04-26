604187684443d8fb0f21e36f1b46c103a40ef11b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-04-26 05:55:47 -0300

    at

--- tests/main_test/calbacks/T_add_func/exec.c
+++ tests/main_test/calbacks/T_add_func/exec.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]){
         printf("error: %s\n",lua_n.get_error_message(l));
     }
    printf("resullt :%lf\n",result);
-    
+
     lua_n.free(l);
 
     return 0;
---
+++ tests/main_test/calbacks/T_return_string/exec.c
@@ -0,0 +1,26 @@
+
+#include "../../../LuaCEmbed.h"
+LuaCEmbedNamespace  lua_n;
+
+
+LuaCEmbedResponse  * test_func(LuaCEmbed *args){
+    return lua_n.response.send_str("str return");
+}
+int main(int argc, char *argv[]){
+
+    lua_n =  newLuaCEmbedNamespace();
+    LuaCEmbed * l = lua_n.newLuaEvaluation();
+    lua_n.add_callback(l,"test",test_func);
+
+
+   char * result = lua_n.get_string_evaluation(l,"test()");
+
+    if(lua_n.has_errors(l)){
+        printf("error: %s\n",lua_n.get_error_message(l));
+    }
+   printf("resullt :%s\n",result);
+    
+    lua_n.free(l);
+
+    return 0;
+}
