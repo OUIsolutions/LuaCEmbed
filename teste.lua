0e12d337132ac8f74e9606d6b5aec54586955dbe
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-01 23:19:18 -0300

    at

--- teste.lua
+++
@@ -1,8 +0,0 @@
-lib = require("my_lib")
-print("long_prop",lib.long_prop)
-print("double_prop",lib.double_prop)
-print("bool_prop",lib.bool_prop)
-print("string_prop",lib.string_prop)
-print("table_prop",lib.table_prop)
-print("table_prop internal",lib.table_prop.test)
-
