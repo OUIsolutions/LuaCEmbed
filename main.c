181ead2d6bb462b835ea842ff9615a218785ab1e
Mateus <mateusmoutinho01@gmail.com>
2024-05-23 20:16:46 -0300

    att

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ LuaCEmbedNamespace  lua_n;
 LuaCEmbedResponse  * add_func(LuaCEmbed *args){
 
 
-    double num1 = lua_n.args.get_long_arg_clojure_evalation(args,0,"function(t) return t.num1  end ");
-    double num2 = lua_n.args.get_long_arg_clojure_evalation(args,0,"function(t) return t.num2  end ");
+    double num1 = lua_n.args.get_long(args,0);
+    double num2 = lua_n.args.get_long(args,0);
 
     if(lua_n.has_errors(args)){
         char *error_message = lua_n.get_error_message(args);
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]){
     lua_n.add_callback(l,"add",add_func);
 
 
-   double result = lua_n.get_evaluation_double(l,"add({num1=10, num2=30})");
+   double result = lua_n.get_evaluation_double(l,"add(10,30)");
 
     if(lua_n.has_errors(l)){
         printf("error: %s\n",lua_n.get_error_message(l));
