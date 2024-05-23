423f44148b86110105b248084234948d9758cd50
Mateus <mateusmoutinho01@gmail.com>
2024-05-23 19:32:39 -0300

    at

--- src/constants/lua_code.h
+++ src/constants/lua_code.h
@@ -5,14 +5,16 @@
 #define PRIVATE_LUA_CEMBED_DEL_PREFIX "__gc"
 #define  PRIVATE_LUA_CEMBED_GLOBAL_EVALUATION_CODE "%s = %s\n"
 
-#define PRIVATE_LUA_CEMBED_MAIN_LIB_TABLE_NAME__ "private_lua_c_embed_main_lib_table%d"
-#define PRIVATE_LUA_CEMBED_MAIN_META_TABLE__ "private_lua_c_embed_main_meta_table_%d"
-#define PRIVATE_LUA_CEMBED_ANONYMOUS_TABLE_ "private_lua_c_embed_anononymous_table_%d_%d"
-#define PRIVATE_LUA_CEMBED_ANONYMOUS_FUNC_TABLE_ "private_lua_c_embed_anononymous_func_table_%d_%d"
-#define PRIVATE_LUA_CEMBED_ARGS_ "private_lua_c_embed_args%d.%d.%d"
-#define  PRIVATE_LUA_CEMBED_MULTIRETURN_ "private_lua_c_embed_multi_return%d.%d_%d"
+
 
 
 #define PRIVATE_LUA_CEMBED_SELFNAME "private_lua_c_embed_self"
 #define PRIVATE_LUA_CEMBED_STAGE_AREA_TABLE "private_lua_c_embed_stage_area_table"
 #define PRIVATE_LUA_CEMBED_TABLE_RETURN "private_lua_embed_table_return"
+
+#define PRIVATE_LUA_CEMBED_MAIN_LIB_TABLE_NAME__ "private_lua_c_embed_main_lib_table_%d"
+#define PRIVATE_LUA_CEMBED_MAIN_META_TABLE__ "private_lua_c_embed_main_meta_table_%d"
+#define PRIVATE_LUA_CEMBED_ANONYMOUS_TABLE_ "private_lua_c_embed_anononymous_table_%d_%d"
+#define PRIVATE_LUA_CEMBED_ANONYMOUS_FUNC_TABLE_ "private_lua_c_embed_anononymous_func_table_%d_%d"
+#define PRIVATE_LUA_CEMBED_ARGS_ "private_lua_c_embed_args_%d_%d"
+#define  PRIVATE_LUA_CEMBED_MULTIRETURN_ "private_lua_c_embed_multi_return_%d_%d"
