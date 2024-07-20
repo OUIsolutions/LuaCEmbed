afd2268fb0275990abd4c5d57bd16a6fa8fe9aab
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-20 17:37:16 -0300

    att

---
+++ build/lua/config.lua
@@ -0,0 +1,21 @@
+START_POINT = "src/one.c"
+
+LINUX_COMPILER = "gcc"
+WINDOWS_COMPILER = "x86_64-w64-mingw32-gcc"
+
+OUTPUT_SINGLE_FILE = "LuaCembed.h"
+END_TEST_POINT= "tests/LuaCembedTest.h"
+SIDE_EFFECT = "tests/target"
+TEST_POINT = "tests/main_test"
+LIB_FOLDER = "src"
+CACHE_POINT  = "cache2"
+EXAMPLES_FOLDER = "exemples"
+TEST_FILE = "exec.c"
+TEST_INPREDICTBILE_START = "S_"
+TEST_PREDICIBLE_START = "T_"
+EXPECTED_NAME= "expected.txt"
+CODEOF_TEXT = "codeof"
+SIDE_EFFECT_COMPARATION = "side_effect"
+
+
+RECONSTRUCT = false
