330fdf1c6bc7ec30d169763416d57dcdd9933fa1
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-20 22:39:53 -0300

    att

--- teste.c
+++ teste.c
@@ -1,38 +1,19 @@
-
 #include "src/one.c"
-
 LuaCEmbedNamespace  lua_n;
 
+int main(int argc, char *argv[]){
+    
+    lua_n =  newLuaCEmbedNamespace();
+    LuaCEmbed * l = lua_n.newLuaEvaluation();
 
-LuaCEmbedResponse  *add_cfunc(LuaCEmbed *args){
-    double first_num = lua_n.args.get_double(args,0);
-    double second_num = lua_n.args.get_double(args,1);
-
-    if(lua_n.has_errors(args)){
-        char *message = lua_n.get_error_message(args);
-        return lua_n.response.send_error(message);
+    lua_n.evaluate(l,"r = 30");
+    long calc = lua_n.get_evaluation_long(l,"r + 20");
+    printf("result %ld",calc);
+    
+    if(lua_n.has_errors(l)){
+        printf("error: %s\n",lua_n.get_error_message(l));
     }
-    double result = first_num + second_num;
-    return lua_n.response.send_double(result);
-}
-LuaCEmbedResponse  *sub_cfunc(LuaCEmbed *args){
-    double first_num = lua_n.args.get_double(args,0);
-    double second_num = lua_n.args.get_double(args,1);
+    lua_n.free(l);
 
-    if(lua_n.has_errors(args)){
-        char *message = lua_n.get_error_message(args);
-        return lua_n.response.send_error(message);
-    }
-    double result = first_num - second_num;
-    return lua_n.response.send_double(result);
-}
-int luaopen_my_lib(lua_State *state){
-    lua_n = newLuaCEmbedNamespace();
-    //functions will be only assescible by the required reciver
-
-    LuaCEmbed * l  = lua_n.newLuaLib(state);
-    lua_n.add_callback(l,"add",add_cfunc);
-    lua_n.add_global_callback(l,"sub",sub_cfunc);
-    return lua_n.perform(l);
-
+    return 0;
 }
