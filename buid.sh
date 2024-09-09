6fd69751ebbd467b691a31bd285170bda3417c1c
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-09 01:27:43 -0300

    att

--- buid.sh
+++ buid.sh
@@ -1,3 +1,3 @@
 ./silverchain.out -src src \
--tags dependencies consts types globals func_declaration func_definition \
---project_short_cut	privateLuaCembed
+-tags dependencies consts  macro types globals fdeclare fdef \
+--project_short_cut	privateLuaCembed --main_name one.c
