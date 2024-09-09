5a06e69a3e06e9b409463210a7952303cf49390a
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-09 01:53:58 -0300

    att

--- src/imports/imports.types.h
+++ src/imports/imports.types.h
@@ -1,6 +1,7 @@
 #include "imports.macros.h"
 #ifndef privateluacembed_types
 #define privateluacembed_types
+#include "../types.typedefs.h"
 #include "../LuaCEmbed/types.LuaCEmbed.h"
 #include "../LuaCEmbedResponse/types.LuaCembedResponse.h"
 #endif
---
+++ src/types.typedefs.h
@@ -0,0 +1,6 @@
+
+//silver_chain_scope_start
+//mannaged by silver chain
+#include "imports/imports.macros.h"
+//silver_chain_scope_end
+typedef long private_lua_cembed_incremented_arg ;
