b1f6eddfc7a973d7439f41e1c11129c3a2784953
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-09 01:32:33 -0300

    att

--- src/imports/imports.types.h
+++ src/imports/imports.types.h
@@ -1,5 +1,6 @@
 #include "imports.macro.h"
 #ifndef privateluacembed_types
 #define privateluacembed_types
+#include "../LuaCEmbed/types.LuaCEmbed.h"
 #include "../LuaCEmbedResponse/types.LuaCembedResponse.h"
 #endif
