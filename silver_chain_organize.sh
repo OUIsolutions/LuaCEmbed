226966e5d40763a016f8026e34c40b698891026e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-17 04:45:58 -0300

    att

--- silver_chain_organize.sh
+++
@@ -1,7 +0,0 @@
-
-
-#these organize the files following the silverchain rules
-## Check https://github.com/OUIsolutions/SilverChain
-silverchain -src src \
--tags dependencies consts  macros types globals fdeclare fdef \
---project_short_cut	privateLuaCembed --watch
