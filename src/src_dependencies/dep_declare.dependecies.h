e679d4a5bfe6acae1ad66984ae5250a50d5a0383
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 20:42:46 -0300

    at

--- src/src_dependencies/dep_declare.dependecies.h
+++ src/src_dependencies/dep_declare.dependecies.h
@@ -5,6 +5,11 @@
 
 #if !defined(lua_h) && !defined(LUA_CEMBED_MOCK_LUA)
     #define LUA_SINGLE_UNITY_GUESS_OS
+    #define LUA_SINGLE_UNITY_EMBED_MODE
+    #if !defined(LUA_CEMBED_NOT_IMPLEMENT_MATH)
+        #define LUA_SINGLE_UNITY_IMPLEMENT_MATH
+    #endif
+
     #include "../../dependencies/lua_single_unity.h"
     #define LUA_CEMBED_LUA_DECLARATED
 #endif 
