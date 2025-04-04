4d2328278535e24f7d0308669d3be2bd1abef1b7
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 20:17:40 -0300

    aat

--- src/src_dependencies/dep_define.dependencies.c
+++ src/src_dependencies/dep_define.dependencies.c
@@ -6,7 +6,7 @@
 //silver_chain_scope_end
 
 #if !defined(LUA_CEMBED_MOCK_LUA_DEFINE) && defined(LUA_CEMBED_LUA_DECLARATED)
-    
+
     #define LUA_SINGLE_UNITY_EMBED_MODE
     #define LUA_SINGLE_UNITY_GUESS_OS
     #if !defined(LUA_CEMBED_NOT_IMPLEMENT_MATH)
