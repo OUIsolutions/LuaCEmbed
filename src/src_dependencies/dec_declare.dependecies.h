b4f6e208086cbbb1c02461b44314ab2f1c1af1a5
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 19:25:58 -0300

    att

--- src/src_dependencies/dec_declare.dependecies.h
+++ src/src_dependencies/dec_declare.dependecies.h
@@ -1,5 +1,8 @@
 
 
+#if !defined(lua_h) && !defined(LUA_CEMBED_MOCK_LUA)
+    #include "../../dependencies/lua_single_unity.h"
+#endif 
 
 
 #if !defined(UniversalGarbage_fdeclare) && !defined(LUA_CEMBED_MOCK_UNIVERSAL_GARBAGE)
