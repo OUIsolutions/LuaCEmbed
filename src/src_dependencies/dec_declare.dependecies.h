80819f12ddd0377593f11c544eee28cde304157c
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 19:06:21 -0300

    att

---
+++ src/src_dependencies/dec_declare.dependecies.h
@@ -0,0 +1,9 @@
+
+
+
+
+#if !defined(UniversalGarbage_fdeclare) && !defined(LUA_CEMBED_MOCK_UNIVERSAL_GARBAGE)
+    #include "../../dependencies/UniversalGarbage.h"
+    #define LUA_CEMBED_UNIVERSAL_GARBAGE_DECLARATED
+#endif
+
---
+++ src/src_dependencies/dep_fine.dependencies.c
@@ -0,0 +1,6 @@
+
+
+
+#if !defined(LUA_CEMBED_MOCK_UNIVERSAL_GARBAGE_DEFINE) && defined(LUA_CEMBED_UNIVERSAL_GARBAGE_DECLARATED)
+#include "../../dependencies/UniversalGarbage.c"
+#endif
