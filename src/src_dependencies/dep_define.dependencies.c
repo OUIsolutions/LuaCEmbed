53bbff50539d94b0edede457ebaa3d5e823ffe74
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-04 20:53:55 -0300

    att

--- src/src_dependencies/dep_define.dependencies.c
+++ src/src_dependencies/dep_define.dependencies.c
@@ -8,7 +8,7 @@
 #if !defined(LUA_CEMBED_MOCK_LUA_DEFINE) && defined(LUA_CEMBED_LUA_DECLARATED)
     #define LUA_SINGLE_UNITY_GUESS_OS
     #define LUA_SINGLE_UNITY_EMBED_MODE
-    #include "../../dependencies/lua_single_unity_classic_onelua.c"
+    //#include "../../dependencies/lua_single_unity_classic_onelua.c"
 
 #endif
 
