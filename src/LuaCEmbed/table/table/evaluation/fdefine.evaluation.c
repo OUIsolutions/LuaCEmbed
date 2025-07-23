507e2d01e31178c85bbc8e164dfba7cf0b74797f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-23 11:17:36 -0300

    att

--- src/LuaCEmbed/table/table/evaluation/fdefine.evaluation.c
+++ src/LuaCEmbed/table/table/evaluation/fdefine.evaluation.c
@@ -1,8 +1,7 @@
 //silver_chain_scope_start
 //mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
-#include "../../../../../imports/imports.dep_define.h"
+#include "../../../../imports/imports.dep_define.h"
 //silver_chain_scope_end
-
 
 LuaCEmbedTable *  LuaCEmbedTable_run_prop_function(LuaCEmbedTable *self,const char *prop,LuaCEmbedTable *args_to_call,int total_returns){
 
